53e149fd0af724b6d48255ff809ed98a4d454ea6
Ivan <ivankulagin93@yandex.ru>
2023-01-12 22:55:08 +0300

    добавил таймер

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -33,4 +33,57 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Таймер
+
+    // срок действия акции
+    let deadline = '2023-01-13';
+
+    // возвращает разницу между сроком действия акии и временем сейчас
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = document.querySelector('.hours'),
+            minutes = document.querySelector('.minutes'),
+            seconds = document.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        // обновляет время на странице
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            // чтобы дата показывалась в формате 00:00:00
+            function addZero(num) {
+                if(num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
