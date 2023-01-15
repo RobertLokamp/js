6febbc42b03c10adc4078d3a9e111b325dcc85ec
Ivan <ivankulagin93@yandex.ru>
2023-01-15 21:59:55 +0300

    добавил модальное окно формы обратной связи

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -86,4 +86,31 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    // Модальное окно
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        descriptionBtn = document.querySelector('.description-btn');
+
+    const openModal = function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    };
+
+    more.addEventListener('click', function() {
+        openModal.call(this);
+    });
+    
+    descriptionBtn.addEventListener('click', function() {
+        openModal.call(this);
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 });
